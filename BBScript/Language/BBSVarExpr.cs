78b753ac6edb7485fcd999339327f08fe6060ca6
= <kp72002@gmail.com>
2024-11-26 12:50:04 -0800

    parse unnamed vars

--- BBScript/Language/BBSVarExpr.cs
+++ BBScript/Language/BBSVarExpr.cs
@@ -9,7 +9,7 @@ public class BBSVarExpr : BBSExpression
     public LexerPosition? Position { get; set; }
     public BBSExpressionType Type => BBSExpressionType.VAR;
     public required string Name { get; init; }
-
+    
     public string Dump()
     {
         return $"(VAR {Name})";
@@ -20,6 +20,10 @@ public class BBSVarExpr : BBSExpression
         context.Bytecode.AddRange(BitConverter.GetBytes(2).ToList());
         if (BBSConfig.Instance.Variables!.TryGetValue(Name, out var value))
             context.Bytecode.AddRange(BitConverter.GetBytes(value).ToList());
+        else if (int.TryParse(Name, out var parsed))
+        {
+            context.Bytecode.AddRange(BitConverter.GetBytes(parsed).ToList());
+        }
         else throw new KeyNotFoundException($"Variable {Name} not found!");
     }
 }
